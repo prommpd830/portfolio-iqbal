cd54e2bdb177a95a6b437d1ab49a376a48ddcf9b
prommpd830 <andimayapada04@gmail.com>
2022-06-13 20:15:07 +0700

    update navbar dan grid line

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         primary: '#9EFF00',
         dark: '#252932',
         secondary: '#969696',
+        colorline: '#3D3F43',
       },
       screens: {
         '2xl': '1320px',
